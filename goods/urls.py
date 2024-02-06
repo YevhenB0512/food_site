324e78815c63cfcc121ec94c342f63b3c3f1177a
YevhenB0512 <evgenijboychuk@gmail.com>
2024-02-06 23:00:38 +0300

    realization urls with slug and categories filter in catalog function

--- goods/urls.py
+++ goods/urls.py
@@ -4,6 +4,6 @@ from goods.views import catalog, product
 app_name = 'goods'
 
 urlpatterns = [
-    path('', catalog, name='index'),
-    path('product/', product, name='product'),
+    path('<slug:category_slug>/', catalog, name='index'),
+    path('product/<slug:product_slug>/', product, name='product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from goods.models import Categories, Products
 
 
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'vse-tovary':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
     context = {
         'title': 'Food delivery - Меню',
         'goods': goods
@@ -11,5 +15,9 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    context = {
+        'product': product
+    }
+    return render(request, 'goods/product.html', context)
