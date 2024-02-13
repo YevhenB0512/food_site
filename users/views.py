26cf4dff1b290cb5c718ea8eba07f214b31c3177
YevhenB0512 <evgenijboychuk@gmail.com>
2024-02-13 13:30:48 +0300

    cart for anonymous user

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+
+from carts.models import Cart
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
@@ -13,10 +15,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username} Вы вошли в аккаунт')
 
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
                     return redirect(request.POST.get('next'))
@@ -36,8 +43,14 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
             messages.success(request, f'{user.username} Вы вошли зарегистрировались и вошли в аккаунт')
             return redirect(reverse('main:index'))
     else:
